822467ab0caaa5b0af8574c02a7cd10a404df6a2
arcenic-creator <zbc25mgko@zbc.dk>
2025-09-11 14:49:33 +0200

    small adition
    i added the pause manager just to make sure everyone has it in the scripts folder

---
+++ Assets/_Scripts/PauseManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public KeyCode pauseKey = KeyCode.Escape;
+    public GameObject pauseMenu;
+    bool isPaused = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            PauseSystem();
+        }
+    }
+
+    public void PauseSystem()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            if (pauseMenu != null)
+            {
+                pauseMenu.SetActive(true);
+            }
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            if (pauseMenu != null)
+            {
+                pauseMenu.SetActive(false);
+            }
+            Time.timeScale = 1f;
+        }
+    }
+}
