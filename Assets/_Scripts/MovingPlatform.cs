5422128f94326ed47e8ac3b91dc4a76e0737a3d3
arcenic-creator <zbc25mgko@zbc.dk>
2025-09-15 11:53:49 +0200

    moving platform added
    just an fyi the one in the prefabs doesnt work, so youre going to have to go ctrl+c the whole platform setup from my scene.

---
+++ Assets/_Scripts/MovingPlatform.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform PointA;
+    public Transform PointB;
+    public float movespeed = 2f;
+
+    private Vector3 nextposition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nextposition = PointB.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, nextposition, movespeed = Time.deltaTime);
+
+        if(transform.position == nextposition)
+        {
+            nextposition = (nextposition == PointA.position) ? PointB.position : PointA.position;
+        }
+    }
+}
+
