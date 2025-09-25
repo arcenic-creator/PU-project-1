1d5628c839bde0fb3bdebfd50e02dc6ba26a547b
blank202p <hamzapkdk@gmail.com>
2025-09-25 13:05:51 +0200

    start menu

---
+++ Assets/_Scripts/Loadlevel.cs
@@ -0,0 +1,25 @@
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loadlevel : MonoBehaviour
+{
+    public int leveltoLoad = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Loadscene(leveltoLoad);
+        }
+    }
+    public void Loadscene(int LevelID)
+    {
+        SceneManager.LoadScene(LevelID);
+    }
+
+    public void quitgame()
+    {
+        Application.Quit();
+    }
+}
