dd4679f88c6c40f773831643f92d38f6605e61e0
blank202p <hamzapkdk@gmail.com>
2025-09-25 09:10:11 +0200

    pause menu

--- Assets/_Scripts/pausebutton.cs
+++ Assets/_Scripts/pausebutton.cs
@@ -1,6 +1,39 @@
 using UnityEngine;
 
-public class pausebutton
+public class pausebutton: MonoBehaviour
 {
+
+	public KeyCode pauseKey = KeyCode.Escape;
+    public GameObject pauseMenu;
+    bool isPaused = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            PauseSystem();
+        }
+    }
+
+    public void PauseSystem()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            if (pauseMenu != null)
+            {
+                pauseMenu.SetActive(true);
+            }
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            if (pauseMenu != null)
+            {
+                pauseMenu.SetActive(false);
+            }
+            Time.timeScale = 1f;
+        }
+    }
     
 }
